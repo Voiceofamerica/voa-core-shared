2fdd9f5519fbea676ef65c2b7abcc4921424d3eb
Doguhan Uluca <duluca@gmail.com>
2017-11-07 10:41:51 -0500

    fix to types, v1.1.0

--- interfaces/iarticle.d.ts
+++ interfaces/iarticle.d.ts
@@ -1,4 +1,4 @@
-import { ContentType } from './icontenttype'
+import { ContentType } from './contenttype'
 
 export enum ArticleVideoRelationship {
   SameItem = 0,
@@ -7,72 +7,70 @@ export enum ArticleVideoRelationship {
 }
 
 export interface IArticle {
-  article: {
+  id: number
+  site: number
+  zone: number
+  /** a-article, v-video or p-photogallery */
+  type: ContentType
+  pubDate: Date
+  lastUpdated: Date
+  url: URL
+  /** Short alternative of the article url. Can be used for Twitter like web site on so on. */
+  twitter: URL
+  title: string
+  introduction: string
+  content: string
+  /** author array */
+  authors: {
+    name: {
+      first: string
+      middle: string
+      last: string
+    }
+    email: string
+    description: string
     id: number
-    site: number
-    zone: number
+  }[]
+  image: {
+    imageTitle: string
+    id: number
+    type: ContentType
+    url: URL
+  }
+  audio: {
+    audioTitle: string
+    audioDescription: string
+    id: number
+    /** seconds */
+    duration: number
+    /** audio/mp3 */
+    mime: MimeType
+    url: string
+    date: string
+  }
+  video: {
+    videoDescription: string
+    guid: string
+    /** Defines relation between article and video => 0=SameItem, 1=MainImage,2=EmbededInContent */
+    relType: ArticleVideoRelationship
+    id: number
+    width: number
+    height: number
+    /** seconds */
+    duration: number
+    url: URL
+    thumbnail: URL
+  }
+  /** story array */
+  relatedStories: {
+    storyTitle: string
+    id: number
+    pubDate: Date
     /** a-article, v-video or p-photogallery */
     type: ContentType
-    pubDate: Date
-    lastUpdated: Date
     url: URL
     /** Short alternative of the article url. Can be used for Twitter like web site on so on. */
     twitter: URL
-    title: string
-    introduction: string
-    content: string
-    /** author array */
-    authors: {
-      name: {
-        first: string
-        middle: string
-        last: string
-      }
-      email: string
-      description: string
-      id: number
-    }[]
-    image: {
-      imageTitle: string
-      id: number
-      type: ContentType
-      url: URL
-    }
-    audio: {
-      audioTitle: string
-      audioDescription: string
-      id: number
-      /** seconds */
-      duration: number
-      /** audio/mp3 */
-      mime: MimeType
-      url: string
-      date: string
-    }
-    video: {
-      videoDescription: string
-      guid: string
-      /** Defines relation between article and video => 0=SameItem, 1=MainImage,2=EmbededInContent */
-      relType: ArticleVideoRelationship
-      id: number
-      width: number
-      height: number
-      /** seconds */
-      duration: number
-      url: URL
-      thumbnail: URL
-    }
-    /** story array */
-    relatedStories: {
-      storyTitle: string
-      id: number
-      pubDate: Date
-      /** a-article, v-video or p-photogallery */
-      type: ContentType
-      url: URL
-      /** Short alternative of the article url. Can be used for Twitter like web site on so on. */
-      twitter: URL
-      thumbnailUrl: URL
-    }[]
-  }
+    thumbnailUrl: URL
+  }[]
 }
